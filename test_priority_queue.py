af6e48700c4306953e0544877b03244c4f7c8b9e
DragonController <dan.h.fowler@gmail.com>
2020-11-22 17:21:22 -0800

    priority_queue test_empty_after_dequeue working

--- test_priority_queue.py
+++ test_priority_queue.py
@@ -96,14 +96,14 @@ class TestPriorityQueue(unittest.TestCase):
         pq.enqueue(Job(1, 'People'))
         self.assertFalse(pq.is_empty())
 
-    # def test_empty_after_dequeue(self):
-    #     """
-    #     A queue with one enqueued value is empty after dequeuing.
-    #     """
-    #     pq = PriorityQueue()
-    #     pq.enqueue(Job(1, 'was'))
-    #     _ = pq.dequeue()
-    #     self.assertTrue(pq.is_empty())
+    def test_empty_after_dequeue(self):
+        """
+        A queue with one enqueued value is empty after dequeuing.
+        """
+        pq = PriorityQueue()
+        pq.enqueue(Job(1, 'was'))
+        _ = pq.dequeue()
+        self.assertTrue(pq.is_empty())
 
     # def test_not_empty_multiple(self):
     #     """
